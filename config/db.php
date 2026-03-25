695c3361b4a144d3501676a012b922fca10f24ce
Roman <odggbo@gmail.com>
2026-03-25 15:10:48 +0300

    Update db.php
    Clean db file

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=genomed',
-    'username' => 'root',
-    'password' => 'tiger',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
